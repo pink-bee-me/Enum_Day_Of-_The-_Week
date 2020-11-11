a3a5b5fbba4812de90facc0921d8d83cb2c13e8d
LaurieSue <pink.bee.me@outlook.com>
2020-11-10 23:41:34 -0600

    Add project files.

---
+++ Enum_Day_Of _The _Week/Enum_DayOfWeek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Day_Of__The__Week
+{
+    public enum Enum_DayOfWeek
+    {
+        Monday,
+        Tuesday, 
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ Enum_Day_Of _The _Week/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Day_Of__The__Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day are you planning on going to the beach?");
+            string value = Console.ReadLine();
+
+            Enum_DayOfWeek day;
+            try
+            {
+                day = (Enum_DayOfWeek)Enum.Parse(typeof(Enum_DayOfWeek), value);
+            }
+            catch (Exception)
+            {
+                
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
